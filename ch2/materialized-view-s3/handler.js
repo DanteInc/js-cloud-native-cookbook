34cb8d601d1d6e10ab2f6f284170baf6b8a87b8e
jgilbert01 <jgilbert01@yahoo.com>
2018-04-17 01:22:26 -0400

    added materialized view s3 recipe

---
+++ ch2/materialized-view-s3/handler.js
@@ -0,0 +1,46 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.listener = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .tap(print)
+    .filter(forThingCreated)
+    .map(toThing)
+    .flatMap(put)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(new Buffer(r.kinesis.data, 'base64'));
+
+const print = v => console.log('%j', v);
+
+const forThingCreated = e => e.type === 'thing-created';
+
+const toThing = event => ({
+  id: event.thing.new.id,
+  name: event.thing.new.name,
+  description: event.thing.new.description,
+  asOf: event.timestamp,
+});
+
+const put = thing => {
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: `things/${thing.id}`,
+    ACL: 'public-read',
+    ContentType: 'application/json',
+    CacheControl: 'max-age=300',
+    Body: JSON.stringify(thing),
+  };
+
+  console.log('params: %j', params);
+
+  const s3 = new aws.S3();
+  return _(s3.putObject(params).promise());
+};
