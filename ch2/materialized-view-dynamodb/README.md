1a02b3cf3590b256907ed6bf20c35caab763225b
jgilbert01 <jgilbert01@yahoo.com>
2018-04-17 00:41:46 -0400

    added materialized view dynamodb recipe

---
+++ ch2/materialized-view-dynamodb/README.md
@@ -0,0 +1,14 @@
+# Creating a materialized view in DynamoDB
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch2/materialized-view-dynamodb --path cncb-materialized-view-dynamodb
+2. cd cncb-materialized-view-dynamodb
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. publish an event from a separate terminal
+   * cd ../cncb-event-stream
+   * sls invoke -r us-east-1 -f publish -s your-name -d '{"type":"thing-created"}'
+7. sls logs -f listener -r us-east-1 -s your-name
+8. sls invoke -r us-east-1 -f query -s your-name -d <id from log>
+9. npm run rm:lcl -- -s your-name
---
+++ ch2/materialized-view-dynamodb/handler.js
@@ -0,0 +1,56 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.listener = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .tap(print)
+    .filter(forThingCreated)
+    .map(toThing)
+    .flatMap(put)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(new Buffer(r.kinesis.data, 'base64'));
+
+const print = v => console.log('%j', v);
+
+const forThingCreated = e => e.type === 'thing-created';
+
+const toThing = event => ({
+  id: event.thing.new.id,
+  name: event.thing.new.name,
+  description: event.thing.new.description,
+  asOf: event.timestamp,
+});
+
+const put = thing => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: thing,
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.put(params).promise());
+};
+
+module.exports.query = (id, context, callback) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: id,
+    },
+  };
+
+  console.log('params: %j', params);  
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.get(params, callback);
+};
