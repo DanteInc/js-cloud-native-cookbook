e8197093f46be84e013a3ecb76cbd18e5935ec99
jgilbert01 <jgilbert01@yahoo.com>
2018-04-18 00:41:20 -0400

    added micro event store recipe

---
+++ ch2/micro-event-store/README.md
@@ -0,0 +1,12 @@
+# Creating a micro event store
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch2/micro-event-store --path cncb-micro-event-store
+2. cd cncb-micro-event-store
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. sls invoke -r us-east-1 -f command -s your-name -d '{"type":"thing-created"}'
+7. sls logs -f listener -r us-east-1 -s your-name
+8. sls logs -f trigger -r us-east-1 -s your-name
+9. npm run rm:lcl -- -s your-name
---
+++ ch2/micro-event-store/handler.js
@@ -0,0 +1,65 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.listener = (event, context, cb) => {
+  // console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .filter(byType)
+    // .tap(print)
+    .flatMap(put)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(new Buffer(r.kinesis.data, 'base64'));
+
+const byType = event => event.type.matches(/thing-.+/);
+
+const put = event => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      partitionKey: event.partitionKey,
+      eventId: event.id,
+      event: event,
+    }
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.put(params).promise());
+};
+
+module.exports.trigger = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .flatMap(getMicroEventStore)
+    .tap(uow => console.log('events: %j', uow))
+    .collect().toCallback(cb);
+};
+
+const getMicroEventStore = (record) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    KeyConditionExpression: '#partitionKey = :partitionKey',
+    ExpressionAttributeNames: {
+      '#partitionKey': 'partitionKey'
+    },
+    ExpressionAttributeValues: {
+      ':partitionKey': record.dynamodb.Keys.partitionKey.S
+    }
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+
+  return _(db.query(params).promise());
+}
+
+const print = v => console.log('%j', v);
