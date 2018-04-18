5020d3c3f852b773d602b1affe5bd7c0a78f566a
jgilbert01 <jgilbert01@yahoo.com>
2018-04-18 16:52:48 -0400

    added materialized view cognito recipe

---
+++ ch2/materialized-view-cognito/README.md
@@ -0,0 +1,16 @@
+# Creating a materialized view in Cognito
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch2/materialized-view-cognito --path cncb-materialized-view-cognito
+2. cd cncb-materialized-view-cognito
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. update index.html file with identityPoolId from previous output
+7. open index.html and copy the identityId
+8. publish an event from a separate terminal
+   * cd ../cncb-event-stream
+   * sls invoke -r us-east-1 -f publish -s your-name -d '{"type":"thing-created","thing":{"new":{"name":"thing five","id":"55555555-5555-5555-5555-555555555555", "identityId":"<identityId from previous step>"}}}'
+9. sls logs -f listener -r us-east-1 -s your-name
+10. press the Synchronize button in index.html
+11. npm run rm:lcl -- -s your-name
---
+++ ch2/materialized-view-cognito/handler.js
@@ -0,0 +1,60 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.listener = (event, context, cb) => {
+  // console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .tap(print)
+    .filter(forThingCreated)
+    .map(toThing)
+    .flatMap(put)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(new Buffer(r.kinesis.data, 'base64'));
+
+const forThingCreated = e => e.type === 'thing-created' && event.thing.identityId;
+
+const toThing = event => ({
+  id: event.thing.new.id,
+  name: event.thing.new.name,
+  description: event.thing.new.description,
+  asOf: event.timestamp,
+  identityId: event.thing.new.identityId
+});
+
+const put = thing => {
+  const params = {
+    IdentityPoolId: process.env.IDENTITY_POOL_ID,
+    IdentityId: thing.identityId,
+    DatasetName: 'things'
+  };
+
+  console.log('params: %j', params);
+
+  const cognitosync = new aws.CognitoSync();
+
+  return _(
+    cognitosync.listRecords(params).promise()
+      .then(data => {
+        params.SyncSessionToken = data.SyncSessionToken;
+        params.RecordPatches = [{
+          Key: 'thing',
+          Value: JSON.stringify(thing),
+          Op: 'replace',
+          SyncCount: data.DatasetSyncCount
+        }];
+
+        return cognitosync.updateRecords(params).promise()
+      })
+  );
+  
+};
+
+const print = v => console.log('%j', v);
+
