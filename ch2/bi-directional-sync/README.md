f8bfefea50254a2dd94cdadc07a3d43a62e90a72
jgilbert01 <jgilbert01@yahoo.com>
2018-04-22 03:03:50 -0400

    added bi directional sync recipe

---
+++ ch2/bi-directional-sync/README.md
@@ -0,0 +1,17 @@
+# Implementing bi-directional synchronization
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch2/bi-directional-sync --path cncb-1-bi-directional-sync
+2. cd cncb-1-bi-directional-sync
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. sls invoke -r us-east-1 -f command -s your-name -d '{"id":"77777777-7777-7777-7777-777777777777","name":"thing seven"}'
+7. sls logs -f command -r us-east-1 -s your-name
+8. sls logs -f trigger -r us-east-1 -s your-name
+9. cd ../cncb-2-bi-directional-sync
+10. sls logs -f listener -r us-east-1 -s your-name
+11. sls logs -f trigger -r us-east-1 -s your-name
+12. sls invoke -r us-east-1 -f query -s your-name -d 77777777-7777-7777-7777-777777777777
+13. reverse and repeat starting step 6 in the 2nd project
+14. npm run rm:lcl -- -s your-name
---
+++ ch2/bi-directional-sync/handler.js
@@ -0,0 +1,135 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.command = (event, context, callback) => {
+  console.log('event: %j', event);
+
+  const thing = Object.assign(
+    {
+      id: uuid.v4(),
+      latch: 'open',
+    },
+    event
+  );
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: thing,
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.put(params, callback);
+};
+
+module.exports.trigger = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .filter(forLatchOpen)
+    .map(toEvent)
+    .tap(print)
+    .flatMap(publish)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const forLatchOpen = e => e.dynamodb.NewImage.latch.S === 'open'; // TODO deleted ???
+
+const toEvent = record => ({
+  id: record.eventID,
+  type: `thing-${EVENT_NAME_MAPPING[record.eventName]}`,
+  timestamp: record.dynamodb.ApproximateCreationDateTime * 1000,
+  partitionKey: record.dynamodb.Keys.id.S,
+  tags: {
+    region: record.awsRegion,
+    source: process.env.SERVERLESS_PROJECT
+  },
+  thing: {
+    old: record.dynamodb.OldImage ?
+      aws.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage) :
+      undefined,
+    new: record.dynamodb.NewImage ?
+      aws.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage) :
+      undefined,
+  },
+});
+
+const EVENT_NAME_MAPPING = {
+  INSERT: 'created',
+  MODIFY: 'updated',
+  REMOVE: 'deleted',
+};
+
+const publish = event => {
+  const params = {
+    StreamName: process.env.STREAM_NAME,
+    PartitionKey: event.partitionKey,
+    Data: new Buffer(JSON.stringify(event)),
+  };
+
+  console.log('params: %j', params);
+
+  const kinesis = new aws.Kinesis();
+  return _(kinesis.putRecord(params).promise());
+}
+
+const print = value => console.log('%j', value);
+
+module.exports.listener = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .tap(print)
+    .filter(forSourceNotSelf)
+    .filter(forThingCrud)
+    .map(toThing)
+    .flatMap(put)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(new Buffer(r.kinesis.data, 'base64'));
+
+const forSourceNotSelf = e => e.tags.source != process.env.SERVERLESS_PROJECT;
+
+const forThingCrud = e => e.type.match(/thing-created|thing-updated/);
+
+const toThing = event => ({
+  id: event.thing.new.id,
+  name: event.thing.new.name,
+  description: event.thing.new.description,
+  timestamp: event.timestamp,
+  latch: 'closed',
+});
+
+const put = thing => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: thing,
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.put(params).promise());
+};
+
+module.exports.query = (id, context, callback) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: id,
+    },
+  };
+
+  console.log('params: %j', params);  
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.get(params, callback);
+};
