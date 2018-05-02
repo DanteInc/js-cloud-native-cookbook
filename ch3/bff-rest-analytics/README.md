bd998a87f5c4c83a785f7f823e1ec43d6303e272
jgilbert01 <jgilbert01@yahoo.com>
2018-05-02 00:43:11 -0400

    added bff analytics recipe

---
+++ ch3/bff-rest-analytics/README.md
@@ -0,0 +1,16 @@
+# Implementing an analytics BFF
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch3/bff-rest-analytics --path cncb-bff-rest-analytics
+2. cd cncb-bff-rest-analytics
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. publish an event from a separate terminal
+   * cd ../cncb-event-stream
+   * sls invoke -r us-east-1 -f publish -s your-name -d '{"type":"purple","partitionKey":"33333333-3333-1111-1111-111111111111"}'
+   * sls invoke -r us-east-1 -f publish -s your-name -d '{"type":"orange","partitionKey":"33333333-3333-1111-1111-111111111111"}'
+   * sls invoke -r us-east-1 -f publish -s your-name -d '{"type":"green","partitionKey":"33333333-3333-1111-2222-111111111111"}'
+7. sls logs -f trigger -r us-east-1 -s your-name
+8. curl https://XYZ.execute-api.us-east-1.amazonaws.com/your-name/query | json_pp
+9. npm run rm:lcl -- -s your-name
---
+++ ch3/bff-rest-analytics/handler.js
@@ -0,0 +1,130 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const moment = require('moment');
+
+module.exports.listener = (event, context, cb) => {
+  // console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .filter(byType)
+    // .tap(print)
+    .flatMap(putEvent)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(Buffer.from(r.kinesis.data, 'base64'));
+
+const byType = event => event.type.match(/.+/); // any
+
+const putEvent = (event) => {
+  const params = {
+    TableName: process.env.EVENTS_TABLE_NAME,
+    Item: {
+      partitionKey: event.partitionKey,
+      timestamp: event.timestamp,
+      event: event,
+      ttl: moment(event.timestamp).add(1, 'h').unix()
+    }
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.put(params).promise());
+};
+
+module.exports.trigger = (event, context, cb) => {
+  // console.log('event: %j', event);
+
+  _(event.Records)
+    .flatMap(getMicroEventStore)
+    .flatMap(store => _(store) // sub-stream
+      .reduce({}, count)
+      // .tap(print)
+      .flatMap(putCounters)
+    )
+    .collect()
+    .toCallback(cb);
+};
+
+const getMicroEventStore = (record) => {
+  const timestamp = moment(Number(record.dynamodb.Keys.timestamp.N));
+  const params = {
+    TableName: process.env.EVENTS_TABLE_NAME,
+    KeyConditionExpression: '#partitionKey = :partitionKey and #timestamp BETWEEN :from and :to',
+    ExpressionAttributeNames: {
+      '#partitionKey': 'partitionKey',
+      "#timestamp": "timestamp"
+    },
+    ExpressionAttributeValues: {
+      ':partitionKey': record.dynamodb.Keys.partitionKey.S,
+      ":from": timestamp.startOf('month'),
+      ":to": timestamp.endOf('month')
+    }
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+
+  return _(
+    db.query(params).promise()
+      .then(data => data.Items)
+  );
+}
+
+const count = (counters, cur) => {
+  // console.log('cur: %j', cur);
+  return Object.assign(
+    {
+      userId: cur.partitionKey,
+      yearmonth: moment(cur.timestamp).format('YYYY-MM'),
+    },
+    counters,
+    {
+      total: counters.total ? counters.total + 1 : 1,
+      [cur.event.type]: counters[cur.event.type] ? counters[cur.event.type] + 1 : 1,
+    }
+  );
+  ;
+}
+
+const putCounters = counters => {
+  const params = {
+    TableName: process.env.VIEW_TABLE_NAME,
+    Item: counters
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.put(params).promise());
+};
+
+module.exports.query = (event, context, cb) => {
+  // console.log('event: %j', event);
+
+  const params = {
+    TableName: process.env.VIEW_TABLE_NAME,
+  };
+
+  // console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+
+  db.scan(params, (err, resp) => {
+    console.log('err: %s', err);
+    console.log('response: %j', resp);
+
+    const response = {
+      statusCode: err ? 500 : 200,
+      body: err ? undefined : JSON.stringify(resp.Items)
+    };
+
+    cb(null, response);
+  });
+};
+
+const print = v => console.log('%j', v);
