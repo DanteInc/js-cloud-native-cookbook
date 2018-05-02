edb14331650efb535ed489a0c3c44308d2042d17
jgilbert01 <jgilbert01@yahoo.com>
2018-05-02 01:48:38 -0400

    added inbound esg recipe

---
+++ ch3/esg-inbound/README.md
@@ -0,0 +1,11 @@
+# Implementing an inbound external service gateway
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch3/esg-inbound --path cncb-esg-inbound
+2. cd cncb-esg-inbound
+3. npm install
+4. npm test
+5. npm run dp:lcl -- -s your-name
+6. https://developer.github.com/webhooks/creating
+7. sls logs -f webhook -r us-east-1 -s your-name
+8. npm run rm:lcl -- -s your-name
---
+++ ch3/esg-inbound/handler.js
@@ -0,0 +1,45 @@
+const aws = require('aws-sdk');
+
+module.exports.webhook = (request, context, callback) => {
+  // console.log('request: %j', request);
+
+  const body = JSON.parse(request.body);
+
+  // console.log('body: %j', body);
+
+  const event = {
+    type: `issue-${body.action}`,
+    id: request.headers['X-GitHub-Delivery'],
+    partitionKey: String(body.issue.id),
+    timestamp: Date.parse(body.issue['updated_at']),
+    tags: {
+      region: process.env.AWS_REGION,
+      repository: body.repository.name,
+    },
+    issue: body, // canonical
+    raw: request
+  };
+
+  console.log('event: %j', event);
+
+  const params = {
+    StreamName: process.env.STREAM_NAME,
+    PartitionKey: event.partitionKey,
+    Data: new Buffer(JSON.stringify(event)),
+  };
+
+  console.log('params: %j', params);
+
+  const kinesis = new aws.Kinesis();
+
+  kinesis.putRecord(params, (err, resp) => {
+    console.log('err: %s', err);
+    console.log('response: %j', resp);
+
+    const response = {
+      statusCode: err ? 500 : 200,
+    };
+  
+    callback(null, response);
+  });  
+};
