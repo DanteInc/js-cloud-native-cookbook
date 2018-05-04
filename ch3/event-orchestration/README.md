1ae5b494e0a8cec0f3e5d22e075932fba4758bc4
jgilbert01 <jgilbert01@yahoo.com>
2018-05-03 23:25:03 -0400

    added event orchestration recipe

---
+++ ch3/event-orchestration/README.md
@@ -0,0 +1,14 @@
+# Orchestrating collaboration between services
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch3/event-orchestration --path cncb-event-orchestration
+2. cd cncb-event-orchestration
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. publish an event from a separate terminal
+   * cd ../cncb-event-stream
+   * sls invoke -r us-east-1 -f publish -s your-name -d ../cncb-event-orchestration/data/order.json
+   * sls invoke -r us-east-1 -f publish -s your-name -d ../cncb-event-orchestration/data/reservation.json
+7. sls logs -f listener -r us-east-1 -s your-name
+8. npm run rm:lcl -- -s your-name
---
+++ ch3/event-orchestration/handler.js
@@ -0,0 +1,90 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+const transitions = [
+  {
+    filter: 'order-submitted',
+    emit: (uow) => ({
+      id: uuid.v1(),
+      type: 'make-reservation',
+      timestamp: Date.now(),
+      partitionKey: uow.event.order.id,
+      reservation: {
+        sku: uow.event.order.sku,
+        quantity: uow.event.order.quantity,
+      },
+      context: {
+        order: uow.event.order,
+        trigger: uow.event.id
+      }
+    })
+  },
+  {
+    filter: 'reservation-confirmed',
+    emit: (uow) => ({
+      id: uuid.v1(),
+      type: 'update-order-status',
+      timestamp: Date.now(),
+      partitionKey: uow.event.context.order.id,
+      order: {
+        status: 'reserved',
+      },
+      context: {
+        reservation: uow.event.reservation,
+        order: uow.event.context.order,
+        trigger: uow.event.id
+      }
+    })
+  },
+];
+
+module.exports.listener = (event, context, cb) => {
+  // console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToUow)
+    // .tap(print)
+    .filter(onTransitions)
+    .flatMap(toEvents)
+    // .tap(print)
+    .flatMap(publish)
+    // .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToUow = r => ({
+  record: r,
+  event: JSON.parse(new Buffer(r.kinesis.data, 'base64')),
+});
+
+const onTransitions = uow => {
+  // find matching transitions
+  uow.transitions = transitions.filter(trans => trans.filter === uow.event.type);
+
+  // proceed forward if there are any matches
+  return uow.transitions.length > 0;
+};
+
+const toEvents = uow => {
+  // create the event to emit for each matching transition
+  return _(uow.transitions.map(t => t.emit(uow)));
+};
+
+const publish = event => {
+  const params = {
+    StreamName: process.env.STREAM_NAME,
+    Record: {
+      PartitionKey: event.partitionKey,
+      Data: new Buffer(JSON.stringify(event)),
+    }
+  };
+
+  print(params);
+
+  const kinesis = new aws.Kinesis();
+  return _(kinesis.putRecord(params).promise());
+}
+
+const print = v => console.log('%j', v);
