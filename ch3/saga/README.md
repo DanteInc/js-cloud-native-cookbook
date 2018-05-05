1cc041d7016442590ef17ef2891521c7a7bf45d8
jgilbert01 <jgilbert01@yahoo.com>
2018-05-05 02:40:53 -0400

    added saga recipe

---
+++ ch3/saga/README.md
@@ -0,0 +1,16 @@
+# Implementing a saga
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch3/saga --path cncb-saga
+2. cd cncb-saga
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. sls invoke -r us-east-1 -f submit -s your-name -p data/order.json
+7. publish an event from a separate terminal
+   * cd ../cncb-event-stream
+   * sls invoke -r us-east-1 -f publish -s your-name -p ../cncb-saga/data/reservation.json
+8. sls invoke -r us-east-1 -f query -s your-name -d 33333333-7777-1111-1111-111111111111
+9. sls logs -f trigger -r us-east-1 -s your-name
+10. sls logs -f listener -r us-east-1 -s your-name
+11. npm run rm:lcl -- -s your-name
---
+++ ch3/saga/handler.js
@@ -0,0 +1,109 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.submit = (request, context, callback) => {
+  console.log('request: %j', request);
+
+  const order = Object.assign(
+    {
+      id: uuid.v4(),
+      status: 'submitted',
+    },
+    request
+  );
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: order,
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.put(params, callback);
+};
+
+module.exports.trigger = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .map(toEvent)
+    .tap(print)
+    .flatMap(publish)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const toEvent = record => ({
+  id: record.eventID,
+  type: `order-submitted`,
+  timestamp: record.dynamodb.ApproximateCreationDateTime * 1000,
+  partitionKey: record.dynamodb.Keys.id.S,
+  tags: {
+    region: record.awsRegion,
+  },
+  order: aws.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage),
+});
+
+const publish = event => {
+  const params = {
+    StreamName: process.env.STREAM_NAME,
+    PartitionKey: event.partitionKey,
+    Data: Buffer.from(JSON.stringify(event)),
+  };
+
+  console.log('params: %j', params);
+
+  const kinesis = new aws.Kinesis();
+  return _(kinesis.putRecord(params).promise());
+}
+
+module.exports.listener = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    // .tap(print)
+    .filter(forReservationViolation)
+    .flatMap(compensate)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(Buffer.from(r.kinesis.data, 'base64'));
+
+const forReservationViolation = e => e.type === 'reservation-violation';
+
+const compensate = event => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: event.context.order.id
+    },
+    AttributeUpdates: {
+      status: { Action: 'PUT', Value: 'cancelled' }
+    },
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.update(params).promise());
+};
+
+module.exports.query = (id, context, callback) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: id,
+    },
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.get(params, callback);
+};
