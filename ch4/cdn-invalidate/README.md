8094250109cb3afdc2932a4b7ef675e5a364ee5d
jgilbert01 <jgilbert01@yahoo.com>
2018-05-20 01:20:59 -0400

    added cdn-invalidate recipe

---
+++ ch4/cdn-invalidate/README.md
@@ -0,0 +1,13 @@
+# Triggering the invalidation of content in a cdn
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch4/cdn-invalidate --path cncb-cdn-invalidate
+2. cd cncb-cdn-invalidate
+3. npm install
+4. npm test
+5. npm run dp:lcl -- -s your-name
+6. sls invoke -r us-east-1 -f load -s your-name -d '{"id":"44444444-7777-1111-1111-000000000000","name":"thing one"}'
+7. curl -v https://xyz.cloudfront.net/things/44444444-7777-1111-1111-000000000000 | json_pp
+8. sls invoke -r us-east-1 -f load -s your-name -d '{"id":"44444444-7777-1111-1111-000000000000","name":"thing one again"}'
+9. curl -v https://xyz.cloudfront.net/things/44444444-7777-1111-1111-000000000000 | json_pp
+10. npm run rm:lcl -- -s your-name
---
+++ ch4/cdn-invalidate/handler.js
@@ -0,0 +1,51 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.load = (thing, context, callback) => {
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: `things/${thing.id}`,
+    ACL: 'public-read',
+    ContentType: 'application/json',
+    CacheControl: 'max-age=31536000', // 1 year
+    Body: JSON.stringify(thing),
+  };
+
+  const s3 = new aws.S3();
+  s3.putObject(params, callback);
+};
+
+module.exports.trigger = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(process.env.DISABLED ? [] : event.Records)
+    .flatMap(messagesToTriggers)
+    .tap(print)
+    .flatMap(invalidate)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const messagesToTriggers = r => _(JSON.parse(r.Sns.Message).Records);
+
+const invalidate = (trigger) => {
+  const params = {
+    DistributionId: process.env.DISTRIBUTION_ID,
+    InvalidationBatch: {
+      CallerReference: uuid.v1(),
+      Paths: {
+        Quantity: 1,
+        Items: [trigger.s3.object.key]
+      }
+    }
+  };
+
+  console.log('params: %j', params);
+
+  const cf = new aws.CloudFront();
+  return _(cf.createInvalidation(params).promise());
+};
+
+const print = v => console.log('%j', v);
