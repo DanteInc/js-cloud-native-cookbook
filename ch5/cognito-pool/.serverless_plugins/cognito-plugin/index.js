6c4c25483d658c19b4885add542f6833ca64569a
jgilbert01 <jgilbert01@yahoo.com>
2018-05-28 01:51:05 -0400

    added cognito-pool recipe

---
+++ ch5/cognito-pool/.serverless_plugins/cognito-plugin/index.js
@@ -0,0 +1,151 @@
+'use strict';
+
+const _ = require('lodash');
+
+class Plugin {
+  constructor(serverless, options) {
+    this.hooks = {
+      'after:deploy:deploy': deploy.bind(null, serverless, options),
+      'before:remove:remove': removeDomain.bind(null, serverless, options),
+    };
+  }
+}
+
+module.exports = Plugin;
+
+const deploy = (serverless) => {
+  return Promise.resolve()
+    .then(() =>
+      getUserPoolId(serverless)
+        .then(data => ({ userPoolId: data }))
+    )
+    .then(uow =>
+      getUserPoolClientId(serverless)
+        .then(data => ({ ...uow, userPoolClientId: data }))
+    )
+    .then(uow =>
+      serverless.getProvider('aws').request('CognitoIdentityServiceProvider', 'describeUserPool', {
+        UserPoolId: uow.userPoolId,
+      })
+        // .then(tap)
+        .then(data => ({ ...uow, pool: { ...data.UserPool } }))
+    )
+    .then(uow => uow.pool.Domain ? uow :
+      serverless.getProvider('aws').request('CognitoIdentityServiceProvider', 'createUserPoolDomain', {
+        UserPoolId: uow.userPoolId,
+        Domain: serverless.service.custom.pool.domain,
+      })
+        // .then(tap)
+        .then(() => uow)
+    )
+    .then(uow =>
+      serverless.getProvider('aws').request('CognitoIdentityServiceProvider', 'updateUserPoolClient', {
+        UserPoolId: uow.userPoolId,
+        ClientId: uow.userPoolClientId,
+        AllowedOAuthFlows: serverless.service.custom.pool.allowedOAuthFlows,
+        AllowedOAuthFlowsUserPoolClient: serverless.service.custom.pool.allowedOAuthFlowsUserPoolClient,
+        AllowedOAuthScopes: serverless.service.custom.pool.allowedOAuthScopes,
+        CallbackURLs: serverless.service.custom.pool.callbackURLs,
+        DefaultRedirectURI: serverless.service.custom.pool.callbackURLs[0],
+        LogoutURLs: serverless.service.custom.pool.logoutURLs,
+        RefreshTokenValidity: serverless.service.custom.pool.refreshTokenValidity,
+        SupportedIdentityProviders: serverless.service.custom.pool.supportedIdentityProviders,
+      })
+        // .then(tap)
+        .then(() => uow)
+    )
+    // .then(uow =>
+    //   serverless.getProvider('aws').request('CognitoIdentityServiceProvider', 'describeUserPool', {
+    //     UserPoolId: uow.userPoolId,
+    //   })
+    //     .then(data => ({ ...uow, pool: { ...data.UserPool } }))
+    // )
+    // .then(uow =>
+    //   serverless.getProvider('aws').request('CognitoIdentityServiceProvider', 'describeUserPoolClient', {
+    //     UserPoolId: uow.userPoolId,
+    //     ClientId: uow.userPoolClientId,
+    //   })
+    //     .then(data => ({ ...uow, client: { ...data.UserPoolClient } }))
+    // )
+    // .then(tap)
+    ;
+};
+
+const removeDomain = (serverless) => {
+  return Promise.resolve()
+    .then(() =>
+      getUserPoolId(serverless)
+        .then(data => ({ userPoolId: data }))
+    )
+    .then(uow => uow.pool.Domain ? uow :
+      serverless.getProvider('aws').request('CognitoIdentityServiceProvider', 'deleteUserPoolDomain', {
+        UserPoolId: uow.userPoolId,
+      })
+        // .then(tap)
+    );
+};
+
+const getUserPoolId = (serverless) => {
+  const awsInfo = _.find(serverless.pluginManager.getPlugins(), (plugin) => {
+    return plugin.constructor.name === 'AwsInfo';
+  });
+
+  if (!awsInfo) {
+    return;
+  }
+
+  return Promise.resolve()
+    .then(() => {
+      if (!awsInfo.gatheredData) {
+        return awsInfo.getStackInfo();
+      }
+    })
+    .then(() => {
+      const outputs = awsInfo.gatheredData.outputs;
+
+      const userPoolId = _.find(outputs, (output) => {
+        return output.OutputKey === 'userPoolId';
+      });
+
+      if (!userPoolId || !userPoolId.OutputValue) {
+        return;
+      }
+
+      return userPoolId.OutputValue;
+    });
+};
+
+const getUserPoolClientId = (serverless) => {
+  const awsInfo = _.find(serverless.pluginManager.getPlugins(), (plugin) => {
+    return plugin.constructor.name === 'AwsInfo';
+  });
+
+  if (!awsInfo) {
+    return;
+  }
+
+  return Promise.resolve()
+    .then(() => {
+      if (!awsInfo.gatheredData) {
+        return awsInfo.getStackInfo();
+      }
+    })
+    .then(() => {
+      const outputs = awsInfo.gatheredData.outputs;
+
+      const userPoolClientId = _.find(outputs, (output) => {
+        return output.OutputKey === 'userPoolClientId';
+      });
+
+      if (!userPoolClientId || !userPoolClientId.OutputValue) {
+        return;
+      }
+
+      return userPoolClientId.OutputValue;
+    });
+};
+
+const tap = (data) => {
+  console.log('data: ', JSON.stringify(data, null, 2));
+  return data;
+}
---
+++ ch5/cognito-pool/README.md
@@ -0,0 +1,10 @@
+# Creating a federated identity pool
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch5/cognito-pool --path cncb-cognito-pool
+2. cd cncb-cognito-pool
+3. npm install
+4. npm test
+5. npm run dp:lcl -- -s your-name
+6. https://cncb-<your-name>.auth.us-east-1.amazoncognito.com/login?redirect_uri=http://localhost:3000/signin&response_type=token&client_id=<client-id>
+7. npm run rm:lcl -- -s your-name
