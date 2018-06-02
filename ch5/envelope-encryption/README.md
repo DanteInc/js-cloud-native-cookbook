0b386c66601e10237cc4f48202cd20bd61b67d75
jgilbert01 <jgilbert01@yahoo.com>
2018-06-02 01:06:31 -0400

    added envelope-encryption recipe

---
+++ ch5/envelope-encryption/README.md
@@ -0,0 +1,13 @@
+# Using envelope encryption
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch5/create-envelope-encryption --path cncb-envelope-encryption
+2. cd cncb-envelope-encryption
+3. npm install
+4. npm test
+5. npm run dp:lcl -- -s your-name
+6. curl -v -X POST -d '{ "id": "55555555-8888-1111-1111-000000000000", "name": "thing1", "description": "This is thing one of two." }' https://XYZ.execute-api.us-east-1.amazonaws.com/john/things
+7. curl -v https://hynj1m6610.execute-api.us-east-1.amazonaws.com/john/things/55555555-8888-1111-1111-000000000000 | json_pp
+8. sls logs -f save -r us-east-1 -s your-name
+9. sls logs -f get -r us-east-1 -s your-name
+10. npm run rm:lcl -- -s your-name
---
+++ ch5/envelope-encryption/handler.js
@@ -0,0 +1,156 @@
+const aws = require('aws-sdk');
+const uuid = require('uuid');
+const CryptoJS = require("crypto-js");
+
+const encrypt = (thing) => {
+  console.log('thing: %j', thing);
+
+  const params = {
+    KeyId: process.env.KEY_ALIAS,
+    KeySpec: 'AES_256',
+  };
+
+  console.log('params: %j', params);
+
+  const kms = new aws.KMS();
+
+  return kms.generateDataKey(params).promise()
+    .then((dataKey) => {
+      // console.log('dataKey: %j', dataKey.KeyId);
+
+      const encryptedThing = Object.keys(thing).reduce((encryptedThing, key) => {
+        if (key !== 'id')
+          encryptedThing[key] = CryptoJS.AES.encrypt(thing[key],
+            dataKey.Plaintext.toString()).toString();
+        return encryptedThing;
+      }, {});
+
+      return {
+        id: thing.id,
+        dataKey: dataKey.CiphertextBlob.toString('base64'),
+        ...encryptedThing,
+      };
+    });
+};
+
+const decrypt = (thing) => {
+  const params = {
+    CiphertextBlob: Buffer.from(thing.dataKey, 'base64'),
+  };
+
+  const kms = new aws.KMS();
+
+  return kms.decrypt(params).promise()
+    .then((dataKey) => {
+      // console.log('dataKey: %j', dataKey.KeyId);
+
+      const decryptedThing = Object.keys(thing).reduce((decryptedThing, key) => {
+        if (key !== 'id' && key !== 'dataKey')
+          decryptedThing[key] = CryptoJS.AES.decrypt(thing[key],
+            dataKey.Plaintext.toString()).toString(CryptoJS.enc.Utf8);
+        return decryptedThing;
+      }, {});
+
+      return {
+        id: thing.id,
+        ...decryptedThing,
+      };
+    });
+};
+
+module.exports.save = (request, context, callback) => {
+  console.log('request: %j', request);
+  // console.log('env: %j', process.env);
+
+  const thing = JSON.parse(request.body);
+  const id = thing.id || uuid.v4();
+
+  encrypt(thing)
+    .then((encryptedThing) => {
+      const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+          id,
+          ...encryptedThing,
+        }
+      };
+
+      console.log('params: %j', params);
+
+      const db = new aws.DynamoDB.DocumentClient();
+
+      return db.put(params).promise();
+    })
+    .then((resp) => {
+      const response = {
+        statusCode: 201,
+        headers: {
+          'access-control-allow-origin': '*',
+          'cache-control': 'no-cache',
+          'location': `https://${request.headers.Host}/${request.requestContext.stage}/things/${id}`
+        },
+      };
+
+      console.log('response: %j', response);
+
+      callback(null, response);
+    })
+    .catch(response500);
+};
+
+module.exports.get = (request, context, callback) => {
+  console.log('request: %j', request);
+
+  const id = request.pathParameters.id;
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id,
+    },
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+
+  db.get(params).promise()
+    .then((resp) => {
+      console.log('resp: %j', resp);
+
+      return resp.Item ? decrypt(resp.Item) : null;
+    })
+    .then((thing) => {
+      console.log('thing: %j', thing);
+
+      const response = {
+        statusCode: !thing ? 404 : 200,
+        headers: {
+          'access-control-allow-origin': '*',
+          'cache-control': 'max-age=3',
+        },
+        body: JSON.stringify(thing)
+      };
+
+      console.log('response: %j', response);
+
+      callback(null, response);
+    })
+    .catch(response500);
+};
+
+const response500 = (err) => {
+  console.log('err: %s', err);
+
+  const response = {
+    statusCode: 500,
+    headers: {
+      'access-control-allow-origin': '*',
+      'cache-control': 'no-cache',
+    },
+  };
+
+  console.log('response: %j', response);
+
+  callback(null, response);
+};
