a84802fac55e1a02fc8c2102f29bbb66750dfffa
jgilbert01 <jgilbert01@yahoo.com>
2018-06-04 23:14:34 -0400

    added dr recipe

---
+++ ch5/dr/README.md
@@ -0,0 +1,17 @@
+# Replicating the data lake for disaster recovery
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch5/dr --path cncb-dr
+2. cd cncb-dr/recovery-account
+3. npm install
+4. npm test -- -s your-name
+5. npm run dp:lcl -- -s your-name
+6. cd ../src1-account
+7. npm install
+8. npm test -- -s your-name
+9. npm run dp:lcl -- -s your-name
+10. sls invoke -r us-east-1 -f load -s your-name
+11. review source and target buckets
+12. npm run rm:lcl -- -s your-name
+13. cd ../recovery-account
+14. npm run rm:lcl -- -s your-name
---
+++ ch5/dr/src1-account/handler.js
@@ -0,0 +1,27 @@
+const aws = require('aws-sdk');
+const moment = require('moment');
+const uuid = require('uuid');
+
+module.exports.load = (thing, context, callback) => {
+    const id = uuid.v1(),
+    const now = moment().utc();
+
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: `stream1/${now.format('YYYY')}/${now.format('MM')}/${now.format('DD')}/${now.format('HH')}/${id}.json`,
+        ContentType: 'application/json',
+        Body: JSON.stringify({
+            id,
+            type: 't1',
+            timestamp: now.valueOf(),
+            tags: {
+                region: process.env.AWS_REGION,
+            },
+        }),
+    };
+
+    console.log('params: %j', params);
+
+    const s3 = new aws.S3();
+    s3.putObject(params, callback);
+};
