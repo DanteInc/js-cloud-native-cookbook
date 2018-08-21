b83ba818264f912442fa75c14d70b3dbf5a6bee4
jgilbert01 <jgilbert01@yahoo.com>
2018-08-20 23:14:56 -0400

    added global table recipe

---
+++ ch10/dynamodb-global-table/README.md
@@ -0,0 +1,17 @@
+# Implement regional replication with DynamoDB
+
+## How to do it...
+1. sls create --template-url https://github.com/danteinc/js-cloud-native-cookbook/tree/master/ch10/dynamodb-global-table --path cncb-dynamodb-global-table
+2. cd cncb-dynamodb-global-table
+3. npm install
+4. npm test -- -s your-name
+5. $ npm run dp:lcl:w -- -s your-name
+6. $ npm run dp:lcl:e -- -s your-name
+7. sls invoke -f command -r us-east-1 -s $MY_STAGE -d '{"id":"77777777-4444-1111-1111-111111111111","name":"thing one"}'
+8. sls invoke -f query -r us-west-2 -s $MY_STAGE -d 77777777-4444-1111-1111-111111111111
+9. $ sls logs -f trigger -r us-east-1 -s $MY_STAGE
+10. $ sls logs -f listener -r us-east-1 -s $MY_STAGE
+11. $ sls logs -f trigger -r us-west-2 -s $MY_STAGE
+12. $ sls logs -f listener -r us-west-2 -s $MY_STAGE
+13. $ npm run rm:lcl:w -- -s your-name
+14. $ npm run rm:lcl:e -- -s your-name
---
+++ ch10/dynamodb-global-table/handler.js
@@ -0,0 +1,147 @@
+const aws = require('aws-sdk');
+const _ = require('highland');
+const uuid = require('uuid');
+
+module.exports.command = (event, context, callback) => {
+  console.log('event: %j', event);
+
+  const thing = Object.assign(
+    {
+      id: uuid.v4(),
+      latch: 'open',
+    },
+    event
+  );
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: thing,
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.put(params, callback);
+};
+
+module.exports.trigger = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .filter(forOrigin)
+    .filter(forLatchOpen)
+    .map(toEvent)
+    .tap(print)
+    .flatMap(publish)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const forOrigin = e => e.dynamodb.NewImage['aws:rep:updateregion'] &&
+  e.dynamodb.NewImage['aws:rep:updateregion'].S === process.env.AWS_REGION;
+
+const forLatchOpen = e => e.dynamodb.NewImage.latch.S === 'open';
+
+const toEvent = record => ({
+  id: record.eventID,
+  type: `thing-${mapEventName(record)}`,
+  timestamp: record.dynamodb.ApproximateCreationDateTime * 1000,
+  partitionKey: record.dynamodb.Keys.id.S,
+  tags: {
+    region: record.awsRegion,
+    source: process.env.SERVERLESS_PROJECT
+  },
+  thing: {
+    old: record.dynamodb.OldImage ?
+      aws.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage) :
+      undefined,
+    new: record.dynamodb.NewImage ?
+      aws.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage) :
+      undefined,
+  },
+});
+
+const EVENT_NAME_MAPPING = {
+  INSERT: 'created',
+  MODIFY: 'updated',
+  REMOVE: 'deleted',
+};
+
+const mapEventName = (record) => {
+  if (!record.dynamodb.OldImage['aws:rep:updateregion']) {
+    return EVENT_NAME_MAPPING.INSERT;
+  }
+  
+  return EVENT_NAME_MAPPING[record.eventName];
+};
+
+const publish = event => {
+  const params = {
+    StreamName: process.env.STREAM_NAME,
+    PartitionKey: event.partitionKey,
+    Data: new Buffer(JSON.stringify(event)),
+  };
+
+  console.log('params: %j', params);
+
+  const kinesis = new aws.Kinesis();
+  return _(kinesis.putRecord(params).promise());
+}
+
+const print = value => console.log('%j', value);
+
+module.exports.listener = (event, context, cb) => {
+  console.log('event: %j', event);
+
+  _(event.Records)
+    .map(recordToEvent)
+    .tap(print)
+    .filter(forSourceNotSelf)
+    .filter(forThingCrud)
+    .map(toThing)
+    .flatMap(put)
+    .tap(print)
+    .collect()
+    .toCallback(cb);
+};
+
+const recordToEvent = r => JSON.parse(new Buffer(r.kinesis.data, 'base64'));
+
+const forSourceNotSelf = e => e.tags.source != process.env.SERVERLESS_PROJECT;
+
+const forThingCrud = e => e.type.match(/thing-created|thing-updated/);
+
+const toThing = event => ({
+  id: event.thing.new.id,
+  name: event.thing.new.name,
+  description: event.thing.new.description,
+  timestamp: event.timestamp,
+  latch: 'closed',
+});
+
+const put = thing => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: thing,
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  return _(db.put(params).promise());
+};
+
+module.exports.query = (id, context, callback) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: id,
+    },
+  };
+
+  console.log('params: %j', params);
+
+  const db = new aws.DynamoDB.DocumentClient();
+  db.get(params, callback);
+};
